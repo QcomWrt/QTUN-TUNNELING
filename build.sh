604a670ff0b3284e0cb1a061aef42a01d2cd6de6
RadBill <52066603+azyanggara@users.noreply.github.com>
2026-04-03 15:24:40 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -r -o -X -ll QTUN-Magisk-$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'CHANGELOG.md' -x 'update.json' -x 'build.sh' -x '.github/*' -x 'QTUN/run/*'
+zip -r -o -X QTUN-Magisk-$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'CHANGELOG.md' -x 'update.json' -x 'build.sh' -x '.github/*' -x 'QTUN/run/*' -x 'QTUN/bin/.bin' -x '.gitignore'
